df1f161ba16d142685d26200e990008d71d7a49f
kk310412040404-pixel <kk310412040404@gmail.com>
2025-12-12 21:44:29 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -51,10 +51,10 @@ conf = ConnectionConfig(
     MAIL_USERNAME="kk310412040404@gmail.com",
     MAIL_PASSWORD="zemhjdtfjcfztvvo",
     MAIL_FROM="kk310412040404@gmail.com",
-    MAIL_PORT=587,
+    MAIL_PORT=465,
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
